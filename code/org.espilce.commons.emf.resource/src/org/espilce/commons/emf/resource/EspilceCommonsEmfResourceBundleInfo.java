c9156f338ebf5ddf8c8eeb765d9fe55574f5cf23
Niko <github@enikao.net>
2018-03-13 23:39:58 +0100

    added BundleInfo interfaces

---
+++ code/org.espilce.commons.emf.resource/src/org/espilce/commons/emf/resource/EspilceCommonsEmfResourceBundleInfo.java
@@ -0,0 +1,16 @@
+package org.espilce.commons.emf.resource;
+
+/**
+ * Information about the bundle.
+ * 
+ * @since 0.0
+ *
+ */
+public interface EspilceCommonsEmfResourceBundleInfo {
+	/**
+	 * Symbolic name (a.k.a. plugin name) of the bundle.
+	 * 
+	 * @since 0.0
+	 */
+	public static final String SYMBOLIC_NAME = "org.espilce.commons.emf.resource";
+}
---
+++ code/org.espilce.commons.emf/src/org/espilce/commons/emf/EspilceCommonsEmfBundleInfo.java
@@ -0,0 +1,16 @@
+package org.espilce.commons.emf;
+
+/**
+ * Information about the bundle.
+ * 
+ * @since 0.0
+ *
+ */
+public interface EspilceCommonsEmfBundleInfo {
+	/**
+	 * Symbolic name (a.k.a. plugin name) of the bundle.
+	 * 
+	 * @since 0.0
+	 */
+	public static final String SYMBOLIC_NAME = "org.espilce.commons.emf";
+}
