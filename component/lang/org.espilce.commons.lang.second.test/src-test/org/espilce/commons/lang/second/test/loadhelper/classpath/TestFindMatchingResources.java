cf6b0a16bbbbb5ce28c294de7948ce1960ce008b
Niko Stotz <niko.stotz@altran.com>
2019-09-30 11:12:54 +0200

    all tests working on maven/windows

--- component/lang/org.espilce.commons.lang.second.test/src-test/org/espilce/commons/lang/second/test/loadhelper/classpath/TestFindMatchingResources.java
+++ component/lang/org.espilce.commons.lang.second.test/src-test/org/espilce/commons/lang/second/test/loadhelper/classpath/TestFindMatchingResources.java
@@ -3,9 +3,9 @@ package org.espilce.commons.lang.second.test.loadhelper.classpath;
 import org.espilce.commons.lang.loadhelper.FilesystemClassloaderLoadHelper;
 import org.espilce.commons.lang.loadhelper.ILoadHelper;
 import org.espilce.commons.lang.test.base.loadhelper.ATestFindMatchingResources;
-import org.junit.Ignore;
+import org.junit.jupiter.api.Disabled;
 
-@Ignore
+@Disabled
 public class TestFindMatchingResources extends ATestFindMatchingResources {
 	@Override
 	protected ILoadHelper createLoadHelper() {
