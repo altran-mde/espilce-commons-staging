b47e402ad32374b295b6ded89da178681d509a01
yblankenstein <yuri.blankenstein@altran.com>
2020-04-10 11:16:05 +0200

    #19 Updating versions and fixing Maven build

---
+++ component/emf.registry/org.espilce.commons.emf.registry.test/src-test/org/espilce/commons/emf/registry/test/TestEPackageRegistryObserver.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (C) 2018 Altran Netherlands B.V.
+ * 
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ ******************************************************************************/
+package org.espilce.commons.emf.registry.test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class TestEPackageRegistryObserver {
+	@Test
+	public void dummy() throws Exception {
+		// TODO: How can we test extension points?
+		assertTrue(true);
+	}
+}
