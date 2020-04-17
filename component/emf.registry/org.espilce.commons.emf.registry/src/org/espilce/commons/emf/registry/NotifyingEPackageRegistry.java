d89b3c4baefb6fee55a6cd76760a1b7445955081
yblankenstein <yuri.blankenstein@altran.com>
2020-04-17 14:34:12 +0200

    #19 Bugfix: getEPackage() alternated the Epckage.Rgistry contents without sending a notification

--- component/emf.registry/org.espilce.commons.emf.registry/src/org/espilce/commons/emf/registry/NotifyingEPackageRegistry.java
+++ component/emf.registry/org.espilce.commons.emf.registry/src/org/espilce/commons/emf/registry/NotifyingEPackageRegistry.java
@@ -9,6 +9,7 @@
  ******************************************************************************/
 package org.espilce.commons.emf.registry;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -30,172 +31,219 @@ import org.eclipse.emf.ecore.EFactory;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.impl.EPackageRegistryImpl;
 
-public class NotifyingEPackageRegistry implements EPackage.Registry {
-	private static final String EXSTENSION_POINT = EspilceCommonsEmfRegistryBundleActivator.SYMBOLIC_NAME
-			+ ".epackage_registry_observer";
-	private static final String ELEMENT_OBSERVER = "observer";
-	private static final String ATTR_CLASS = "class";
-	private static final String ATTR_URI = "uri";
+public class NotifyingEPackageRegistry implements EPackage.Registry, Serializable {
+	private static final long serialVersionUID = -8041178609149335697L;
 
-	// Delegating instead of extending to ensures that default Map implementations
-	// will be invoked
-	private final EPackage.Registry delegate;
-
-	public NotifyingEPackageRegistry() {
-		this(new EPackageRegistryImpl());
+	// Delegating instead of extending to ensures that default java.util.Map
+	// implementations will be invoked instead of optimized HashMap implementation
+	// which alternates the Map contents directly
+	private final EPackage.Registry delegate = new NotifyingEPackageRegistryImpl();
+	
+	public EPackage getEPackage(String nsURI) {
+		return delegate.getEPackage(nsURI);
 	}
 
-	protected NotifyingEPackageRegistry(EPackage.Registry delegate) {
-		this.delegate = delegate;
+	public EFactory getEFactory(String nsURI) {
+		return delegate.getEFactory(nsURI);
 	}
 
-	@Override
-	public Object put(String uri, Object value) {
-		final Object previousValue = delegate.put(uri, value);
-		if (previousValue != value) {
-			final List<Notification> notificationChain = new ArrayList<>(2);
-			addNotification(Notification.Type.UNREGISTERED, uri, previousValue, notificationChain);
-			addNotification(Notification.Type.REGISTERED, uri, value, notificationChain);
-			notifyObservers(notificationChain);
-		}
-		return previousValue;
-	}
-
-	@Override
-	public void putAll(Map<? extends String, ? extends Object> map) {
-		// Too much effort (also performance) to build a notification chain
-		// as we need to match the previous values with the new values,
-		// so just delegate to put
-		map.entrySet().forEach(e -> put(e.getKey(), e.getValue()));
-	}
-
-	@Override
-	public Object remove(Object uri) {
-		final Object previousValue = delegate.remove(uri);
-		if (previousValue != null) {
-			final List<Notification> notificationChain = new ArrayList<>(1);
-			addNotification(Notification.Type.UNREGISTERED, uri, previousValue, notificationChain);
-			notifyObservers(notificationChain);
-		}
-		return previousValue;
-	}
-
-	@Override
-	public void clear() {
-		final List<Notification> notificationChain = new ArrayList<>(delegate.size());
-		for (Map.Entry<String, Object> entry : delegate.entrySet()) {
-			addNotification(Notification.Type.UNREGISTERED, entry.getKey(), entry.getValue(), notificationChain);
-		}
-		delegate.clear();
-		notifyObservers(notificationChain);
-	}
-
-	private void addNotification(Notification.Type type, Object uri, Object ePackage,
-			List<Notification> notificationChain) {
-		if (type != null && uri instanceof String && ePackage instanceof EPackage) {
-			notificationChain.add(new Notification(type, (String) uri, (EPackage) ePackage));
-		}
-	}
-
-	private void notifyObservers(List<Notification> notificationChain) {
-		if (notificationChain == null || notificationChain.isEmpty()) {
-			// Skip if no notifications need to be send
-			return;
-		}
-		IExtensionRegistry registry = Platform.getExtensionRegistry();
-		IExtensionPoint point = registry.getExtensionPoint(EXSTENSION_POINT);
-		if (point == null) {
-			return;
-		}
-		for (IExtension extension : point.getExtensions()) {
-			for (IConfigurationElement observerElement : extension.getConfigurationElements()) {
-				notifyObserver(observerElement, notificationChain);
-			}
-		}
-	}
-
-	private void notifyObserver(IConfigurationElement observerElement, List<Notification> notificationChain) {
-		if (!observerElement.isValid() || !ELEMENT_OBSERVER.equals(observerElement.getName())) {
-			return;
-		}
-		final String uri = observerElement.getAttribute(ATTR_URI);
-		final Iterator<Notification> notifications;
-		if (uri == null) {
-			// Send all notifications
-			notifications = notificationChain.iterator();
-		} else {
-			// Filter notifications for matching uri
-			notifications = notificationChain.stream().filter(n -> Objects.equals(uri, n.uri)).iterator();
-		}
-		if (!notifications.hasNext()) {
-			// Skip if no (matching) notifications need to be send
-			return;
-		}
-		try {
-			EPackageRegistryObserver observer = EPackageRegistryObserver.class
-					.cast(observerElement.createExecutableExtension(ATTR_CLASS));
-			notifications.forEachRemaining(n -> n.notify(observer));
-		} catch (CoreException e) {
-			EspilceCommonsEmfRegistryBundleActivator.getLog()
-					.log(new Status(IStatus.ERROR, EspilceCommonsEmfRegistryBundleActivator.SYMBOLIC_NAME,
-							String.format("Failed to notify EPackageRegistryObserver of plug-in %s: %s",
-									observerElement.getContributor().getName(), observerElement.getAttribute(ATTR_CLASS)),
-							e));
-		}
-	}
-
-	// Prevent alternations to the map
-
-	@Override
-	public Set<String> keySet() {
-		return Collections.unmodifiableSet(delegate.keySet());
-	}
-
-	@Override
-	public Collection<Object> values() {
-		return Collections.unmodifiableCollection(delegate.values());
-	}
-
-	@Override
-	public Set<Entry<String, Object>> entrySet() {
-		return Collections.unmodifiableSet(delegate.entrySet());
-	}
-
-	// Plain delegate methods
-
-	@Override
 	public int size() {
 		return delegate.size();
 	}
 
-	@Override
 	public boolean isEmpty() {
 		return delegate.isEmpty();
 	}
 
-	@Override
-	public boolean containsKey(Object uri) {
-		return delegate.containsKey(uri);
+	public boolean containsKey(Object key) {
+		return delegate.containsKey(key);
 	}
 
-	@Override
 	public boolean containsValue(Object value) {
 		return delegate.containsValue(value);
 	}
 
-	@Override
-	public Object get(Object uri) {
-		return delegate.get(uri);
+	public Object get(Object key) {
+		return delegate.get(key);
+	}
+
+	public Object put(String key, Object value) {
+		return delegate.put(key, value);
+	}
+
+	public Object remove(Object key) {
+		return delegate.remove(key);
+	}
+
+	public void putAll(Map<? extends String, ? extends Object> m) {
+		delegate.putAll(m);
+	}
+
+	public void clear() {
+		delegate.clear();
+	}
+
+	public Set<String> keySet() {
+		return delegate.keySet();
+	}
+
+	public Collection<Object> values() {
+		return delegate.values();
+	}
+
+	public Set<Entry<String, Object>> entrySet() {
+		return delegate.entrySet();
 	}
 
 	@Override
-	public EPackage getEPackage(String uri) {
-		return delegate.getEPackage(uri);
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((delegate == null) ? 0 : delegate.hashCode());
+		return result;
 	}
 
 	@Override
-	public EFactory getEFactory(String uri) {
-		return delegate.getEFactory(uri);
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		NotifyingEPackageRegistry other = (NotifyingEPackageRegistry) obj;
+		if (delegate == null) {
+			if (other.delegate != null)
+				return false;
+		} else if (!delegate.equals(other.delegate))
+			return false;
+		return true;
+	}
+
+	private static final class NotifyingEPackageRegistryImpl extends EPackageRegistryImpl {
+		private static final long serialVersionUID = -5377442334573160215L;
+
+		private static final String EXSTENSION_POINT = EspilceCommonsEmfRegistryBundleActivator.SYMBOLIC_NAME
+				+ ".epackage_registry_observer";
+
+		private static final String ELEMENT_OBSERVER = "observer";
+		private static final String ATTR_CLASS = "class";
+		private static final String ATTR_URI = "uri";
+		
+		@Override
+		public Object put(String uri, Object value) {
+			final Object previousValue = super.put(uri, value);
+			if (previousValue != value) {
+				final List<Notification> notificationChain = new ArrayList<>(2);
+				addNotification(Notification.Type.UNREGISTERED, uri, previousValue, notificationChain);
+				addNotification(Notification.Type.REGISTERED, uri, value, notificationChain);
+				notifyObservers(notificationChain);
+			}
+			return previousValue;
+		}
+
+		@Override
+		public void putAll(Map<? extends String, ? extends Object> map) {
+			// Too much effort (also performance) to build a notification chain
+			// as we need to match the previous values with the new values,
+			// so just delegate to put
+			map.entrySet().forEach(e -> put(e.getKey(), e.getValue()));
+		}
+
+		@Override
+		public Object remove(Object uri) {
+			final Object previousValue = super.remove(uri);
+			if (previousValue != null) {
+				final List<Notification> notificationChain = new ArrayList<>(1);
+				addNotification(Notification.Type.UNREGISTERED, uri, previousValue, notificationChain);
+				notifyObservers(notificationChain);
+			}
+			return previousValue;
+		}
+
+		@Override
+		public void clear() {
+			final List<Notification> notificationChain = new ArrayList<>(super.size());
+			for (Map.Entry<String, Object> entry : super.entrySet()) {
+				addNotification(Notification.Type.UNREGISTERED, entry.getKey(), entry.getValue(), notificationChain);
+			}
+			super.clear();
+			notifyObservers(notificationChain);
+		}
+		
+		// Prevent direct alternations to the map. Enforce that alternations are only
+		// done via put/remove methods as listed above
+
+		@Override
+		public Set<String> keySet() {
+			return Collections.unmodifiableSet(super.keySet());
+		}
+
+		@Override
+		public Collection<Object> values() {
+			return Collections.unmodifiableCollection(super.values());
+		}
+
+		@Override
+		public Set<Entry<String, Object>> entrySet() {
+			return Collections.unmodifiableSet(super.entrySet());
+		}
+		
+		// Notification support
+
+		private void addNotification(Notification.Type type, Object uri, Object ePackage,
+				List<Notification> notificationChain) {
+			if (type != null && uri instanceof String && ePackage instanceof EPackage) {
+				notificationChain.add(new Notification(type, (String) uri, (EPackage) ePackage));
+			}
+		}
+
+		private void notifyObservers(List<Notification> notificationChain) {
+			if (notificationChain == null || notificationChain.isEmpty()) {
+				// Skip if no notifications need to be send
+				return;
+			}
+			IExtensionRegistry registry = Platform.getExtensionRegistry();
+			IExtensionPoint point = registry.getExtensionPoint(EXSTENSION_POINT);
+			if (point == null) {
+				return;
+			}
+			for (IExtension extension : point.getExtensions()) {
+				for (IConfigurationElement observerElement : extension.getConfigurationElements()) {
+					notifyObserver(observerElement, notificationChain);
+				}
+			}
+		}
+
+		private void notifyObserver(IConfigurationElement observerElement, List<Notification> notificationChain) {
+			if (!observerElement.isValid() || !ELEMENT_OBSERVER.equals(observerElement.getName())) {
+				return;
+			}
+			final String uri = observerElement.getAttribute(ATTR_URI);
+			final Iterator<Notification> notifications;
+			if (uri == null) {
+				// Send all notifications
+				notifications = notificationChain.iterator();
+			} else {
+				// Filter notifications for matching uri
+				notifications = notificationChain.stream().filter(n -> Objects.equals(uri, n.uri)).iterator();
+			}
+			if (!notifications.hasNext()) {
+				// Skip if no (matching) notifications need to be send
+				return;
+			}
+			try {
+				EPackageRegistryObserver observer = EPackageRegistryObserver.class
+						.cast(observerElement.createExecutableExtension(ATTR_CLASS));
+				notifications.forEachRemaining(n -> n.notify(observer));
+			} catch (CoreException e) {
+				EspilceCommonsEmfRegistryBundleActivator.getLog()
+						.log(new Status(IStatus.ERROR, EspilceCommonsEmfRegistryBundleActivator.SYMBOLIC_NAME,
+								String.format("Failed to notify EPackageRegistryObserver of plug-in %s: %s",
+										observerElement.getContributor().getName(), observerElement.getAttribute(ATTR_CLASS)),
+								e));
+			}
+		}
 	}
 
 	private static final class Notification {
